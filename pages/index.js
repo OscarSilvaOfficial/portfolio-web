c49616ab60d13522e112eeb2d23f6efae0d4e7c4
OscarSilvaOfficial <oscarkaka222@gmail.com>
2021-06-29 22:36:27 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Me from '../components/Me' 
 import Head from '../components/Head'
+import Description from '../components/Description'
 
 export default function HomePage() {
   return (
     <>
       <Head title="Oscar da Silva" iconSrc="/" />
       <Me />
+      <Description />
     </>
   )
 }
