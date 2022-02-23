683d16bc02e850897666a8000be48aa275b45013
OscarSilvaOfficial <oscarkaka222@gmail.com>
2022-02-23 00:14:28 -0300

    Ajuste nos retrys

--- nuxt.config.js
+++ nuxt.config.js
@@ -114,6 +114,14 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
+    retry: { 
+      retries: 100,
+      retryDelay: (retryCount) => {
+        console.debug(`retry attempt: ${retryCount}`);
+        return retryCount * 1000; // time interval between retries
+      },
+    }
+    
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
